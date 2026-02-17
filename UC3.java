0e09591f042087e3c5a19eb8cf44bcfa23088120
hitwesh <hiteshroy0001@gmail.com>
2026-02-18 00:49:39 +0530

    Set UC3.java

--- UC3.java
+++ UC3.java
@@ -1,4 +1,4 @@
-public class UC3 {
+public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println(String.join(" ", " OOOO ", " OOOO ", "PPPP  ", " SSSS "));
         System.out.println(String.join(" ", "O    O", "O    O", "P   P ", "S     "));
