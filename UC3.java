a82ba8d7cec917f05af0cfee42cfc0d0cbe7f723
Hitesh Kumar Roy <165147068+hitwesh@users.noreply.github.com>
2026-02-18 00:53:05 +0530

    add UC3

--- UC3.java
+++ UC3.java
@@ -1,4 +1,4 @@
-public class OOPSBannerApp {
+public class UC3 {
     public static void main(String[] args) {
         System.out.println(String.join(" ", " OOOO ", " OOOO ", "PPPP  ", " SSSS "));
         System.out.println(String.join(" ", "O    O", "O    O", "P   P ", "S     "));
